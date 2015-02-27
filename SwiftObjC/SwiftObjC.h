1456f05cda2529f445e6e4e1d85fd3ee144339dc
Elliott Minns <elliott.minns@me.com>
2015-02-27 09:39:01 +0000

    Initial Commit

---
+++ SwiftObjC/SwiftObjC.h
@@ -0,0 +1,19 @@
+//
+//  SwiftObjC.h
+//  SwiftObjC
+//
+//  Created by Elliott Minns on 27/02/2015.
+//  Copyright (c) 2015 Elliott Minns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftObjC.
+FOUNDATION_EXPORT double SwiftObjCVersionNumber;
+
+//! Project version string for SwiftObjC.
+FOUNDATION_EXPORT const unsigned char SwiftObjCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftObjC/PublicHeader.h>
+
+
